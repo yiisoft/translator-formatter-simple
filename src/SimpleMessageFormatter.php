71e4f1c20c0b61813a3703ff7cd76e28a06dcb8d
Anton Samoylenko <a.samoylenko95@gmail.com>
2021-03-31 20:59:18 +0300

    Set ErrorLevel 1 for Psalm

--- src/SimpleMessageFormatter.php
+++ src/SimpleMessageFormatter.php
@@ -11,6 +11,7 @@ class SimpleMessageFormatter implements MessageFormatterInterface
     public function format(string $message, array $parameters, string $locale): string
     {
         $replacements = [];
+        /** @var mixed $value */
         foreach ($parameters as $key => $value) {
             if (is_scalar($value)) {
                 $replacements['{' . $key . '}'] = $value;
